cc006991334daef2558ecf832c266a38491246d9
wangsl <wangshuailin754@gmail.com>
2017-07-16 12:13:11 +0800

    update

--- sw.js
+++ sw.js
@@ -19,8 +19,8 @@ const PRECACHE_LIST = [
   "./js/bootstrap.min.js",
   "./js/hux-blog.min.js",
   "./js/snackbar.js",
-  "./img/icon_wechat.png",
-  "./img/avatar-hux.jpg",
+  "./img/advatar.jpg",
+  "./img/avatar.jpg",
   "./img/home-bg.jpg",
   "./img/404-bg.jpg",
   "./css/hux-blog.min.css",
@@ -32,7 +32,7 @@ const PRECACHE_LIST = [
 ]
 const HOSTNAME_WHITELIST = [
   self.location.hostname,
-  "huangxuan.me",
+  "wangsl123.github.io",
   "yanshuo.io",
   "cdnjs.cloudflare.com"
 ]
